63c425ad8927f5fa80528583809127fc45d7ab19
Marc <marc@webactix.com>
2019-07-22 16:17:32 +1200

    Cleaning up examples

--- bootstrap.php
+++ bootstrap.php
@@ -3,16 +3,8 @@
 include 'vendor/autoload.php';
 
 // Set up our identity provider, giving it the clientId, clientSecret and redirectUri that ThriveCart have sent you as the application developer
-if(false) {
-	$provider = new \ThriveCart\Oauth([
-	    'clientId'                => 'example-client-1',
-	    'clientSecret'            => 'examplepass',
-	    'redirectUri'             => 'http://localhost/thrivecart-api-demo/oauth_example.php', // URL to be redirected to after OAuth acceptance
-	]);
-} else {
-	$provider = new \ThriveCart\Oauth([
-	    'clientId'                => 'example-client-1',
-	    'clientSecret'            => 'examplepass',
-	    'redirectUri'             => 'http://localhost/thrivecart-api-demo/oauth_example.php', // URL to be redirected to after OAuth acceptance
-	]);
-}
+$provider = new \ThriveCart\Oauth([
+    'clientId'                => 'example-client-1',
+    'clientSecret'            => 'examplepass',
+    'redirectUri'             => 'http://localhost/thrivecart-api-demo/oauth_example.php', // URL to be redirected to after OAuth acceptance
+]);
